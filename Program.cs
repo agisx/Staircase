1807e62a7080f68a7883d439f81d68fb92679c29
LAPTOP-RT0RDR2Q\HP <agilobitajm@gmail.com>
2023-06-23 06:49:40 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = 6;
+for (int i = 0; i < n; i++)
+{
+    string space = new(' ', n - i - 1);
+    string hastag = new('#', i + 1);
+    string result = space + hastag;
+    Console.WriteLine(result);
+}
